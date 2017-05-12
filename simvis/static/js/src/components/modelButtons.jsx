87ea8c7104aea49ca44fd29376ca2e7a9cf654d6
Alex <ahd985@gmail.com>
2017-05-12 07:38:49 -0400

    WIP

---
+++ simvis/static/js/src/components/modelButtons.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+
+export const cellButtonIcon = <svg viewBox="0 0 100 100" width="100" height="100">
+        <rect height="100"
+              style={{opacity:1, fill:"url(#pattern_node-3)", fillOpacity:1, stroke:"#000000"}}
+              width="100" x="0" y="0" />
+        <defs>
+            <pattern id="pattern_node-3" x="0" y="0" width="100%" height="100%" patternContentUnits="userSpaceOnUse">
+                <rect id="rect273" x="0" width="100%" height="100%" y="0" style={{fill: "rgb(252, 78, 42)", opacity: 1}} />
+                <rect id="rect275" x="0" width="100%" height="1px" fill="#FFFFFF" y="-1%" style={{opacity: 1}} />
+                <rect id="rect277" x="0" width="100%" height="100%" fill="none" y="0" style={{opacity: 1}} />
+                <rect id="rect279" x="0" width="100" height="100" y="80" style={{opacity:1, fill:"#fd8d3c"}}>
+                    <animate attributeName="y" values="80;20;80" dur="3s" repeatCount="indefinite" />
+                    <animate attributeName="height" values="20;80;20" dur="3s" repeatCount="indefinite" />
+                </rect>
+                <rect id="rect281" x="0" width="100" height="1" y="80" style={{opacity:1, fill:"#ffffff"}}>
+                    <animate attributeName="y" values="80;20;80" dur="3s" repeatCount="indefinite" />
+                </rect>
+                <rect id="rect283" x="0" width="100%" height="100%" fill="none" y="224.4543531994629" style={{opacity: 1}} />
+            </pattern>
+        </defs>
+    </svg>;
+
+
+export const heatmapButtonIcon = 0;
+
+export const lineButtonIcon = 0;
+
+export const legendButtonIcon = 0;
+
+export const reportButtonIcon = 0;
+
+export const tableButtonIcon = 0;
